5853bbce0aef4768454aafcd0aabf77972f680fb
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-01-15 16:33:38 +0000

    added winnings calculation

--- horse-race/bet.rb
+++ horse-race/bet.rb
@@ -2,8 +2,8 @@ def place_bet
   gets.chomp.to_i
 end
 
-def calculate_winnings
-  rand(0..2.5)
+def calculate_winnings(bet_amount)
+  bet_amount + rand(0..2.5).round(2)
 end
 
 
--- horse-race/interface.rb
+++ horse-race/interface.rb
@@ -21,19 +21,24 @@ unless horses.empty?
   puts 'Place your bet:'
   print '> '
   bet_amount = place_bet
+  total -= bet_amount
   race(horses)
   say_result(horses)
 
   if picked_horse == horses[0]
+    bet_amount = calculate_winnings(bet_amount) + 5
     puts 'Contratulations! Your horse got 1st place!'
-    puts "You won"
+    puts "You won #{bet_amount}€"
   elsif picked_horse == horses[1]
+    bet_amount = calculate_winnings(bet_amount) + 2
     puts 'Contratulations! Your horse got 2nd place!'
-    puts "You won"
+    puts "You won #{bet_amount}€"
   elsif picked_horse == horses[2]
+    bet_amount = calculate_winnings(bet_amount) + 0.5
     puts 'Contratulations! Your horse got 3rd place!'
-    puts "You won"
-  else puts 'Your horse didn\' make the podium. You lost!'
+    puts "You won #{bet_amount}€"
+  else
+    puts 'Your horse didn\'t make the podium. You lost!'
   end
 end
 
